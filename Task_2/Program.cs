faff624cebbaae4462ec294567de6240f2a9d3bc
Yuliya <i@yzubkova.ru>
2023-01-19 13:30:09 +0300

    Задача два про проверку квадрата числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число  ");
+string number1Str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1Str);
+
+Console.WriteLine("Введите второе число  ");
+string number2Str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2Str);
+
+if(number1/number2 == number2)
+{
+    Console.WriteLine("Да, первое число является квадратом второго.");
+}
+
+else 
+{
+    Console.WriteLine("Нет, первое число не является квадратом второго.");
+}
