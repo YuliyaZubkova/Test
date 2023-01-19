791f035453eb433c416baea76686a26fcadcb603
Yuliya <i@yzubkova.ru>
2023-01-19 15:06:19 +0300

    Задача 4 про список чисел от -N до N

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число N а на выход выдает все целые числа от -N до N
+
+Console.WriteLine("Введите число N  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+for (int i = -number; i <= number; i++)
+{
+Console.WriteLine(i);
+}
