3cbd639e1687e273e73b4a5ef68c2d5366046978
Yuliya <i@yzubkova.ru>
2023-01-19 16:00:18 +0300

    Добавили задачу 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая на вход принимает трехзначное число и на выходе выдает последнюю цифру этого числа
+
+Console.WriteLine("Введите любое трехзначное число  ");
+int number = Int32.Parse(Console.ReadLine());
+if(number > 999 && number < 100)
+{
+    Console.WriteLine("Введен неправильный номер. Введите любое трехзначное число  ");
+    return;
+}
+
+int result = number % 10;
+Console.WriteLine(result);
