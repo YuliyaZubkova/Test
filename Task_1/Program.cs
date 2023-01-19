b89bf6b2330eddae45783ff6766d6cc8dcbd23f1
Yuliya <i@yzubkova.ru>
2023-01-19 12:50:48 +0300

    Задача1-Квадрат числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+то что мы вводим с консоли всегда будет тип string
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
+//Console.WriteLine("Квадрат числа" + number + "=" + result);
+//Console.WriteLine("Квадрат числа {0} = {1}", number, result);
